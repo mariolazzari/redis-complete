77c4d785d244eb0baa0be7d48d93632156883f9d
Mario Lazzari <mario.lazzari@gmail.com>
2026-07-11 15:37:11 +0200

    Done lesson 167

--- rbay/src/services/queries/items/search.ts
+++ rbay/src/services/queries/items/search.ts
@@ -9,4 +9,22 @@ export const searchItems = async (term: string, size: number = 5) => {
 		.split(' ')
 		.map((word) => (word ? `%${word}%` : ''))
 		.join(' ');
+
+	// Look at cleaned and make sure it is valid
+	if (cleaned === '') {
+		return [];
+	}
+
+	const query = `(@name:(${cleaned}) => { $weight: 5.0 }) | (@description:(${cleaned}))`;
+
+	// Use the client to do an actual search
+	const results = await client.ft.search(itemsIndexKey(), query, {
+		LIMIT: {
+			from: 0,
+			size
+		}
+	});
+
+	// Deserialize and return the search results
+	return results.documents.map(({ id, value }) => deserialize(id, value as any));
 };
